d73f8e2f99646c080937e0e36eb4014c1fb1c532
Paradox401 <sandeshsapkota371@gmail.com>
2024-07-04 09:31:22 +0545

    Your commit message

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+
+const Home = () => {
+  return (
+    <div>
+      <Header></Header>
+      <ExploreMenu></ExploreMenu>
+    </div>
+  )
+}
+
+export default Home
