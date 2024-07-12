e6d4af5b41cccead5041db132fa0f7c809cfb48f
Paradox401 <sandeshsapkota371@gmail.com>
2024-07-12 18:26:07 +0545

    added cart

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
 
 export const Navbar = ({setShowLogin}) => {
 const [menu,setMenu] = useState("menu");
 
+const {getTotalCartAmount} = useContext(StoreContext)
+
   return (
     <div className='navbar'>
-        <img className = 'logo'src={assets.logo} alt="" />
+        <Link to='/'><img className = 'logo'src={assets.logo} alt="" /></Link>
         <ul className="navbar-menu">
             <Link to='/' onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>HOME</Link>
             <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>MENU</a>
@@ -19,8 +22,8 @@ const [menu,setMenu] = useState("menu");
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
-                <div className="dot"></div>
+                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+                <div className={getTotalCartAmount()===0?"":"dot"}></div>
             </div>
             <button onClick={()=>setShowLogin(true)}>sign in</button>
         </div>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
 const Cart = () => {
-const {cartItems,food_list,removeFromCart} = useContext(StoreContext);
+const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext);
 
   return (
     <div className='cart'>
@@ -17,6 +17,55 @@ const {cartItems,food_list,removeFromCart} = useContext(StoreContext);
         </div>
         <br />
         <hr />
+        {food_list.map((item,index)=>{
+          if(cartItems[item._id]>0)
+          {
+            return(            
+              <div>
+                <div className="cart-items-title cart-items-item ">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>Rs{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>Rs{item.price*cartItems[item._id]}</p>
+                  <p onClick={()=>removeFromCart(item._id)} className='cross'>X</p>
+              </div>
+              <hr />
+              </div>
+            )
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+          <div className="cart-total-details">
+            <p>Subtotal</p>
+            <p>RS{getTotalCartAmount()}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <p>Delivery Fee</p>
+            <p>RS{30}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <b>Total</b>
+            <b>RS{getTotalCartAmount()+30}</b>
+          </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code, Enter it here</p>
+            <div className='cart-promocode-input'>
+                <input type="text" placeholder='Promo Code' />
+                <button>Apply</button>
+            </div>
+          </div>
+        </div>
       </div>
       </div>
   )
